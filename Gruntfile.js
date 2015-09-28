7774725318662ccbbff26c0eacc5dc8d5cc201b9
beshan <beshan@bigmir.net>
2015-09-29 00:49:37 +0300

    upd

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,25 +4,14 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('clean1',
+    grunt.registerTask('clean',
         [
-            'clean:www1'
+            'clean:www'
         ]);
-    grunt.registerTask('clean2',
-        [
-            'clean:www2'
-        ]);
-    grunt.registerTask('clean3',
-        [
-            'clean:www3'
-        ]);
-
-
 
     grunt.registerTask('jenkins',
         [
-            'clean:www',
-            'copy:www'
+            'clean:www'
         ]);
 
     grunt.initConfig({
@@ -33,9 +22,7 @@ module.exports = function (grunt) {
             options: {
                 force: true
             },
-            www1: ['<%= pkg.projectDir.www %>'],
-            www2: ['<%= pkg.rootDir.www %>'],
-            www3: ['<%= pkg.packDir.www %>']
+            www: ['<%= pkg.projectDir.www %>']
         },
 
         copy: {
