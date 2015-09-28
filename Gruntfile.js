30ac530e4ad02200209e79123190870171f91f80
beshan <beshan@bigmir.net>
2015-09-28 20:19:57 +0300

    node modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,36 @@
-/**
- * Created  NB
- */
+module.exports = function (grunt) {
+
+//    grunt.loadNpmTasks('grunt-cordovacli');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('jenkins',
+        [
+            'clean:www',
+            'copy:www'
+        ]);
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        clean: {
+            options: {
+                force: true
+            },
+            www: ['<%= pkg.projectDir.www %>']
+        },
+
+        copy: {
+            www: {
+                expand: true,
+                cwd: '<%= pkg.rootDir.app %>',
+                src: '**',
+                dest: '<%= pkg.rootDir.www %>'
+            }
+
+        }
+
+    });
+
+};
